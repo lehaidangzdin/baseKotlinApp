e80359b4080944d02c3b3dd69074ae0c3263374e
danglh <danglh@unicloud.com.vn>
2023-06-09 13:25:38 +0700

    abc

--- app/src/main/java/com/lhd/androidbase/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/lhd/androidbase/ui/home/HomeViewModel.kt
@@ -1,7 +1,9 @@
 package com.lhd.androidbase.ui.home
 
 import com.lhd.androidbase.base.viewmodel.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class HomeViewModel @Inject constructor() : BaseViewModel() {
 }
