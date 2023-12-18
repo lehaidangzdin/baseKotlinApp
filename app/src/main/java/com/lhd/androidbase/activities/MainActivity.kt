16c2d42d88f7d463b0a20e655dfc732a08591318
danglh <danglh@unicloud.com.vn>
2023-12-18 17:58:46 +0700

    add job scheduler

--- app/src/main/java/com/lhd/androidbase/activities/MainActivity.kt
+++ app/src/main/java/com/lhd/androidbase/activities/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.lhd.androidbase.activities
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -7,13 +12,17 @@ import android.widget.FrameLayout
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.lhd.androidbase.R
 import com.lhd.androidbase.base.activities.BaseActivity
+import com.lhd.androidbase.ui.example_service.JobSchedulerService
 import dagger.hilt.android.AndroidEntryPoint
-import kotlin.math.log
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
 
     private var loadingLayout: FrameLayout? = null
+
+    companion object {
+        private const val PERIODIC_TIME: Long = 15 * 60 * 1000
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.e("Frank", "MainActivity")
@@ -21,10 +30,34 @@ class MainActivity : BaseActivity() {
         installSplashScreen()
         setContentView(R.layout.activity_main)
         loadingLayout = findViewById(R.id.loadingLayout)
+        //
+        scheduleJob(this)
     }
 
     override fun showLoading(isShow: Boolean) {
         loadingLayout?.visibility = if (isShow) View.VISIBLE else View.GONE
+    }
+
+    private fun scheduleJob(context: Context) {
+        val componentName = ComponentName(context, JobSchedulerService::class.java)
+        val jobInfoBuilder = JobInfo.Builder(JobSchedulerService.ID_JOB, componentName)
+            .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY) // Điều kiện mạng
+//            .setPersisted(true)// Lưu trạng thái của công việc sau khi khởi động lại hệ thống
+//            .setPeriodic(5000)
+//            .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+//            .setRequiresDeviceIdle(false)
+//            .setRequiresCharging(true)
+            .setPersisted(true)
+            .setPeriodic(PERIODIC_TIME)
+
+        // Cài đặt thêm các điều kiện và tùy chọn khác nếu cần.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            jobInfoBuilder.setRequiresBatteryNotLow(true) // Yêu cầu pin có thể sử dụng
+        }
+
+        val jobInfo = jobInfoBuilder.build()
+        val jobScheduler = context.getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+        jobScheduler.schedule(jobInfo)
     }
 
 
---
+++ app/src/main/java/com/lhd/androidbase/ui/example_service/JobSchedulerService.kt
@@ -0,0 +1,50 @@
+package com.lhd.androidbase.ui.example_service
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.util.Log
+import android.widget.Toast
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class JobSchedulerService : JobService() {
+
+    private val serviceJob = Job()
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + serviceJob)
+    override fun onStartJob(p0: JobParameters?): Boolean {
+        Toast.makeText(applicationContext, "Service running!", Toast.LENGTH_SHORT).show()
+
+        coroutineScope.launch {
+            doAsyncTask(p0)
+        }
+
+        return true
+    }
+
+    private suspend fun doAsyncTask(params: JobParameters?) {
+//        for (i in 1..10) {
+//            Log.e(TAG, "doAsyncTask:  $i")
+//            delay(1000)
+//        }
+        Log.e(TAG, "doAsyncTask: abc")
+        delay(15000)
+        Log.e(TAG, "doAsyncTask: abc-done")
+        jobFinished(params, false)
+    }
+
+    override fun onStopJob(p0: JobParameters?): Boolean {
+        Log.e(TAG, "onStopJob: ")
+        Toast.makeText(applicationContext, "Service stop!", Toast.LENGTH_SHORT).show()
+        serviceJob.cancel()
+        return true
+    }
+
+
+    companion object {
+        const val TAG = "JobService"
+        const val ID_JOB = 1
+    }
+}
